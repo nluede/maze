f07264ee3cd6bc7cc3a20b35d828d6e94c24ba1c
Niels Lüdemann <niels@nlue.de>
2018-10-06 19:29:11 +0200

    Added tests for maze.Set

--- maze/maze_test.go
+++ maze/maze_test.go
@@ -11,8 +11,7 @@ func TestNew(t *testing.T) {
 		height int
 	}
 
-	oneByOne := createStructure(1, 1)
-	emptyStructure := [][]bool{}
+	var emptyStructure [][]bool
 
 	tests := []struct {
 		name string
@@ -20,17 +19,22 @@ func TestNew(t *testing.T) {
 		want Maze
 	}{
 		{
-			name: "Negative size creates zero sized maze",
-			args: args{width: 1, height: 1},
-			want: Maze{width: 1, height: 1, structure: oneByOne},
+			name: "Maze with given size is created in given size",
+			args: args{width: 1, height: 3},
+			want: Maze{width: 1, height: 3, structure: createStructure(1, 3)},
 		},
 		{
-			name: "Negative size creates zero sized maze",
+			name: "Maze with just one field gets created",
+			args: args{width: 1, height: 1},
+			want: Maze{width: 1, height: 1, structure: createStructure(1, 1)},
+		},
+		{
+			name: "Negative size parameters create zero sized maze",
 			args: args{width: -1, height: -1},
 			want: Maze{width: 0, height: 0, structure: emptyStructure},
 		},
 		{
-			name: "Zero size creates zero sized maze",
+			name: "Zero as parameter creates a zero sized maze",
 			args: args{width: 0, height: 0},
 			want: Maze{width: 0, height: 0, structure: emptyStructure},
 		},
@@ -44,10 +48,113 @@ func TestNew(t *testing.T) {
 	}
 }
 
+func TestSet(t *testing.T) {
+	type args struct {
+		x         int
+		y         int
+		isPassway bool
+	}
+	tests := []struct {
+		name  string
+		given Maze
+		args  args
+		want  Maze
+	}{
+		{
+			name: "Passway is set correctly on a maze without any passways",
+			given: createMazeFromString(2, 3,
+				""+
+					"XX"+
+					"XX"+
+					"XX"),
+			args: args{x: 2, y: 2, isPassway: true},
+			want: createMazeFromString(2, 3,
+				""+
+					"XX"+
+					"X_"+
+					"XX"),
+		},
+		{
+			name: "Wall is set correctly on a maze without any walls",
+			given: createMazeFromString(2, 3,
+				""+
+					"__"+
+					"__"+
+					"__"),
+			args: args{x: 2, y: 2, isPassway: false},
+			want: createMazeFromString(2, 3,
+				""+
+					"__"+
+					"_X"+
+					"__"),
+		},
+		{
+			name: "When a wall is set to be a wall, it stays a wall",
+			given: createMazeFromString(2, 3,
+				""+
+					"XX"+
+					"XX"+
+					"XX"),
+			args: args{x: 2, y: 2, isPassway: false},
+			want: createMazeFromString(2, 3,
+				""+
+					"XX"+
+					"XX"+
+					"XX"),
+		},
+		{
+			name: "When a passway is set to be a passway, it stays a passway",
+			given: createMazeFromString(2, 3,
+				""+
+					"XX"+
+					"X_"+
+					"XX"),
+			args: args{x: 2, y: 2, isPassway: true},
+			want: createMazeFromString(2, 3,
+				""+
+					"XX"+
+					"X_"+
+					"XX"),
+		}, {
+			name: "Field at 0,0 can be set",
+			given: createMazeFromString(2, 2,
+				""+
+					"XX"+
+					"XX"),
+			args: args{x: 1, y: 1, isPassway: true},
+			want: createMazeFromString(2, 2,
+				""+
+					"_X"+
+					"XX"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			maze := tt.given
+			maze.Set(tt.args.x, tt.args.y, tt.args.isPassway)
+
+		})
+	}
+}
+
 func createStructure(width int, height int) [][]bool {
 	result := make([][]bool, height)
 	for i := range result {
 		result[i] = make([]bool, width)
 	}
 	return result
+}
+
+func createMazeFromString(width, height int, input string) Maze {
+	maze := New(width, height)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			currentCharPosition := y*width + x
+			currentChar := input[currentCharPosition]
+			if currentChar == '_' {
+				maze.Set(x, y, false)
+			}
+		}
+	}
+	return maze
 }
